0d6d48236e064e48d24db021706f023d51e3c756
Mina Emad <148348808+MinaEmad193@users.noreply.github.com>
2024-06-23 03:27:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+let phones = document.querySelectorAll(".phones-category")
+let mainPhone = document.querySelector(".current-phone")
+mainPhone.attributes.src.nodeValue = localStorage.getItem("main phone")
+document.body.className = localStorage.getItem("current mood")
+
+function currentPhone(src) {
+    mainPhone.setAttribute("src", src)
+    
+    
+    if (src === "./images/0.png") {
+        mood = "gold"
+    }
+    if (src === "./images/1.png") {
+        mood = "blue"
+    }
+    if (src === "./images/2.png") {
+        mood = "black"
+    }
+    if (src === "./images/3.png") {
+        mood = "grey"
+    }
+    if (src === "./images/4.png") {
+        mood = "red"
+    }
+    localStorage.setItem("current mood", mood)
+    localStorage.setItem("main phone", src)
+
+    document.body.className = localStorage.getItem("current mood")
+}
+
